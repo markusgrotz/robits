f4ccfb839ce61e5ef3b82107bb4bbb55fd418289
Markus Grotz <grotz@uw.edu>
2025-07-13 12:34:18 +0200

    add pose2mat and mat2pose convenience functions

--- robits/utils/transform_utils.py
+++ robits/utils/transform_utils.py
@@ -8,7 +8,7 @@ def transform_pose(
     transform: np.ndarray, position: np.ndarray, quaternion: np.ndarray
 ) -> Tuple[np.ndarray, np.ndarray]:
     """
-    :param transform: The transformation to apply
+    :param transform: The 4x4 matrix transformation to apply
     :param position: The position to transform
     :param quaternion: The quaternion to transform
     """
@@ -18,3 +18,31 @@ def transform_pose(
     rotation = R.from_matrix(R_matrix)
     quaternion_transformed = (rotation * R.from_quat(quaternion)).as_quat()
     return position_transformed, quaternion_transformed
+
+
+def pose2mat(pose: Tuple[np.ndarray, np.ndarray]) -> np.ndarray:
+    """
+    Converts a pose tuple to a 4x4 matrix
+    Quaternion format is xyzw
+
+    :param pose: (position, quaternion)
+    :returns: the transformation matrix
+    """
+    position, quaternion = pose
+    mat = np.identity(4)
+    mat[:3, :3] = R.from_quat(quaternion).as_matrix()
+    mat[:3, 3] = position
+    return mat
+
+
+def mat2pose(mat: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Converts a 4x4 matrix to a pose tuple
+
+    :param mat: the transformation matrix
+    :returns: (position, quaternion)
+    """
+    position = mat[:3, 3]
+    quaternion = R.from_matrix(mat[:3, :3]).as_quat()
+    return position, quaternion
+
